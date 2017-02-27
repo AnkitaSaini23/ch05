d29f90432e7fea5a90efeac140989de2e58e5481
Ankita Saini <as3323@njit.edu>
2017-02-27 15:02:46 -0500

    Changes to product and image test1

--- product_manager/product_list.php
+++ product_manager/product_list.php
@@ -34,11 +34,11 @@
                 <td><?php echo $product['productName']; ?></td>
                 <td class="right"><?php echo $product['listPrice']; ?></td>
                 <td><form action="." method="post">
-                    <input type="hidden" name="action"
+                    <input  name="action"
                            value="delete_product">
-                    <input type="hidden" name="product_id"
+                    <input name="product_id"
                            value="<?php echo $product['productID']; ?>">
-                    <input type="hidden" name="category_id"
+                    <input  name="category_id"
                            value="<?php echo $product['categoryID']; ?>">
                     <input type="submit" value="Delete">
                 </form></td>
